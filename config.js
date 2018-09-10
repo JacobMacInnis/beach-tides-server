cbb84bdd9ffefe1617eda5d77878f5eb22cc05ad
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-09-10 13:39:01 -0400

    fixed config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/noteful',
   TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost/noteful-test',
-  CLIENT_ORIGIN: 'https://calm-temple-68738.herokuapp.com/' || 3000,
+  CLIENT_ORIGIN: 'https://calm-temple-68738.herokuapp.com' || 3000,
 };
