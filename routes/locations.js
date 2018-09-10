741105e5f311603450abbaff6af1e467e8c78698
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-09-10 15:38:57 -0400

    added date functionality to location route

--- routes/locations.js
+++ routes/locations.js
@@ -2,21 +2,28 @@
 
 const express = require('express');
 const request = require('request-promise');
+const timestamp = require('unix-timestamp');
+const moment = require('moment');
 
 const Location = require('../models/location');
+
 
 const router = express.Router();
 
 const worldTidesKey = 'd584b43d-2bd4-40cb-999c-634e60d8202a';
 
 router.get('/', (req, res, next) => {
-  
   const { location, date } = req.query;
   let filter = {};
-  
   if (location) filter.zip_code = location;
   let lat, lon, city, state;
-  
+  let dateParams = '';
+
+  if (!(date === moment().format('YYYY-MM-DD'))) {
+    let futureDate = timestamp.fromDate(date);
+    dateParams = `&start=${futureDate}`;
+  }
+  let WorldTideURL;
   Location.findOne(filter)
     .then(location => {
       if (location === null || undefined) {
@@ -30,9 +37,14 @@ router.get('/', (req, res, next) => {
       state = location.state;
     })
     .then(() => {
+      if (dateParams === '') {
+        WorldTideURL = `https://www.worldtides.info/api?extremes&lat=${lat}&lon=${lon}&key=${worldTidesKey}`;
+      } else {
+        WorldTideURL = `https://www.worldtides.info/api?extremes&lat=${lat}&lon=${lon}&key=${worldTidesKey}${dateParams}`;
+      }
       return request(
         {
-          url: `https://www.worldtides.info/api?extremes&lat=${lat}&lon=${lon}&key=${worldTidesKey}`,
+          url: WorldTideURL
         });
     })
     .then(response => {
